df9b191ce8b85a651f398cdb693053d32fddf94e
Ayman <ayman2002rahman@gmail.com>
2024-04-09 04:14:14 -0500

    Implemented checking for a match in the sand

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 import pygame
 import sys
+import time
 from tetris_env import Tetris_Env, CELL_SIZE
 
 DISPLAY_SIZE = (1000, 1000)
 GAME_POSITION = (150, 200)
-WIDTH = 100
-HEIGHT = 160
+WIDTH = 37
+HEIGHT = 60
 BORDER_THICKNESS = 5
+
+FLICKER_FREQUENCY = 3
 
 pygame.init()
 screen = pygame.display.set_mode(DISPLAY_SIZE)
@@ -25,14 +28,32 @@ def draw_game(env):
     pygame.draw.rect(screen, (255, 255, 255), (env.position[0]-BORDER_THICKNESS, env.position[1]-BORDER_THICKNESS, 2*BORDER_THICKNESS+env.size[0]*CELL_SIZE, 2*BORDER_THICKNESS+env.size[1]*CELL_SIZE))
     # playable area:
     pygame.draw.rect(screen, (0, 0, 0), (env.position[0], env.position[1], env.size[0]*CELL_SIZE, env.size[1]*CELL_SIZE))
+    
+    # sand
     for y in range(env.size[1]):
         for x in range(env.size[0]):
             if env.sand[y][x] is not None:
                 pixel = env.sand[y][x]
                 pygame.draw.rect(screen, pixel.rgb, (env.position[0]+x*CELL_SIZE, env.position[1]+y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
+    # tetromino
+    
+def draw_match_visual(env, visited):
+    for x, y in visited:
+        pygame.draw.rect(screen, (255, 255, 255), (env.position[0]+x*CELL_SIZE, env.position[1]+y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+def override_match_visual(env, visited):
+    for x, y in visited:
+        pygame.draw.rect(screen, (0, 0, 0), (env.position[0]+x*CELL_SIZE, env.position[1]+y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
 def main():
     env = Tetris_Env(GAME_POSITION, (WIDTH, HEIGHT))
+
+    flickering = False
+    visited = None
+    flick_frame = 3
+    flicks = 0
+    draw_white = False
 
     while True:
 
@@ -42,9 +63,32 @@ def main():
                 sys.exit()
 
         draw_game(env)
-        env.step(None)
+
+        if not flickering:
+            visited = env.step(None)
+            if visited:
+                flickering = True
+        else:
+            if flicks <= FLICKER_FREQUENCY:
+                if draw_white:
+                    draw_match_visual(env, visited)
+                    flick_frame -= 1
+                    if flick_frame <= 0:
+                        flick_frame = 3
+                        draw_white = False
+                else:
+                    override_match_visual(env, visited)
+                    flick_frame -= 1
+                    if flick_frame <= 0:
+                        flick_frame = 3
+                        draw_white = True
+                        flicks += 1
+            else:
+                flicks = 0
+                flickering = False
+
         pygame.display.flip()
-        #clock.tick(60)
+        clock.tick(30)
 
 if __name__ == '__main__':
     main()
--- tetris_env.py
+++ tetris_env.py
@@ -26,26 +26,89 @@ class Pixel():
         self.rgb = rgb
         self.position = position # (x, y) tuple
 
+# Tried to make a tetrominoe class but didn't really work
+# class Tetrominoe():
+
+#     def __init__(self, size):
+#         # self.block = random.choice['O', 'L', 'J', 'T', 'I', 'S', 'Z']
+#         self.block = 'T'
+#         self.size = size
+#         #sand will contain only the current sand pixels
+#         self.sand = [[None for j in range(self.size[0])] for i in range(self.size[1])]
+
+#     def draw_block(self):
+#         block_color = Color.random_color()
+#         color_rgb = {1: (53, 89, 144), 2: (164, 65, 47), 3: (97, 152, 74), 4: (204, 154, 52), 5: (145, 128, 196), 6: (255, 127, 39)}
+#         if self.block == 'T':
+#             for i in range(0, 12):
+#                 for j in range(44, 56):
+#                     self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+#             for i in range(12, 25):
+#                 for j in range(32, 68):
+#                     self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+
 class Tetris_Env():
 
     tetrominoe_set = set()
 
     def __init__(self, position, size):
         self.position = position
-        self.size = size
+        self.size = size # (width, height)
         #sand will contain only the current sand pixels
         self.sand = [[None for j in range(self.size[0])] for i in range(self.size[1])]
+        TETROMINOE_SIZE = 12 # even number please
+        # default block:
         block_color = Color.random_color()
-        print(block_color)
         color_rgb = {1: (53, 89, 144), 2: (164, 65, 47), 3: (97, 152, 74), 4: (204, 154, 52), 5: (145, 128, 196), 6: (255, 127, 39)}
-        for i in range(0, 12):
-            for j in range(12, 24):
-                self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
-        for i in range(12, 25):
-            for j in range(0, 36):
-                self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        tetrominoe = random.choice(['O', 'L', 'J', 'T', 'I', 'S', 'Z'])
+        tetrominoe = 'T'
+        if tetrominoe == 'O':
+            for i in range(0, TETROMINOE_SIZE*2):
+                for j in range(int(size[0]/2) - TETROMINOE_SIZE, int(size[0]/2) + TETROMINOE_SIZE):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'L':
+            for i in range(0, TETROMINOE_SIZE*3):
+                for j in range(int(size[0]/2) - TETROMINOE_SIZE, int(size[0]/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+            for i in range(TETROMINOE_SIZE*2, TETROMINOE_SIZE*3):
+                for j in range(int(size[0]/2), int(size[0]/2) + TETROMINOE_SIZE):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'J':
+            for i in range(0, TETROMINOE_SIZE*3):
+                for j in range(int(size[0]/2), int(size[0]/2) + TETROMINOE_SIZE):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+            for i in range(TETROMINOE_SIZE*2, TETROMINOE_SIZE*3):
+                for j in range(int(size[0]/2) - TETROMINOE_SIZE, int(size[0]/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'T':
+            for i in range(0, TETROMINOE_SIZE):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE/2), int(size[0]/2) + int(TETROMINOE_SIZE/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+            for i in range(TETROMINOE_SIZE, (TETROMINOE_SIZE*2)+1):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE*3/2), int(size[0]/2) + int(TETROMINOE_SIZE*3/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'I':
+            for i in range(0, TETROMINOE_SIZE*4):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE/2), int(size[0]/2) + int(TETROMINOE_SIZE/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'S':
+            for i in range(0, TETROMINOE_SIZE):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE/2), int(size[0]/2) + int(TETROMINOE_SIZE*3/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+            for i in range(TETROMINOE_SIZE, TETROMINOE_SIZE*2):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE*3/2), int(size[0]/2) + int(TETROMINOE_SIZE/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+        elif tetrominoe == 'Z':
+            for i in range(0, TETROMINOE_SIZE):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE*3/2), int(size[0]/2) + int(TETROMINOE_SIZE/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+            for i in range(TETROMINOE_SIZE, TETROMINOE_SIZE*2):
+                for j in range(int(size[0]/2) - int(TETROMINOE_SIZE/2), int(size[0]/2) + int(TETROMINOE_SIZE*3/2)):
+                    self.sand[i][j] = Pixel(block_color, color_rgb[block_color], (j, i))
+
         self.tetrominoe = None
         self.next_tetrominoe = None
+        self.score = 0
 
     def reset(self):
         pass
@@ -59,7 +122,13 @@ class Tetris_Env():
         # 5) 
         pass
 
+    def pixel_at(self, x, y): # helper function to get the sandf pizxel at i=y and j=x
+        return self.sand[y][x]
+
     def step(self, action):
+        # 1.) handle action logic
+        # 2.) update sand physics 
+        # 3.) check for a match
 
         def set_cell(x, y, pixel):
             self.sand[y][x] = pixel
@@ -88,10 +157,74 @@ class Tetris_Env():
 
             if down or left or right:
                 set_cell(x, y, None)
+ 
+        def bfs(staring_y):
+            match = False
 
-        def check_match(): # this helper function will help find a connection path from both ends to determine a match ha sbeen made
-            pass
+            search_color = self.pixel_at(0, staring_y).color
+            visited = set()
+            queue = [(0, staring_y)] # stores x, y pairs to represent pixel's position
+            while len(queue) > 0: # may need to change the logic if we append pixles to the queue and check their validitiy after they are popped from it (queue would be larger)
+                x, y = queue.pop(0)
 
-        for y in range(self.size[1]-1, -1, -1):
+                # Check to see if pixel needs to be ignored
+                if (
+                    x < 0 or x >= self.size[0] or y < 0 or y >= self.size[1] or # out of bounds
+                    (x, y) in visited or # seen
+                    self.pixel_at(x, y) is None or # no pixel
+                    self.pixel_at(x, y).color is not search_color # wrong color
+                ):
+                    continue
+
+                visited.add((x, y))
+
+                # check for goal (if a connection has been made, then the goal will be a pixel on the right wall)
+                if x == self.size[0]-1:
+                    match = True
+
+                # check all 4 directions
+                queue.append((x, y-1)) # up 
+                queue.append((x+1, y)) # right 
+                queue.append((x, y+1)) # down 
+                queue.append((x-1, y)) # left 
+
+            if match:
+                return visited
+            else:
+                return None
+
+        def left_wall():
+            wall = [] # from bottom to top, store the first i index for each unique color section
+            current_color = None
+            for y in range(self.size[1]-1, -1, -1):
+                pixel = self.pixel_at(0, y)
+                if pixel is None:
+                    break
+                if pixel.color is not current_color:
+                    wall.append(y)
+                    current_color = pixel.color
+            return wall
+                
+        # --- STEP FUNCTION LOGIC ---
+
+        # 1.) handle input action logic here
+
+        # 2.) sand physics
+        for y in range(self.size[1]-1, -1, -1): # bottom up
             for x in range(self.size[0]-1, -1, -1):
                 update_cell(x, y)
+        
+        # 3.) check for a match (currently only handles one match at a time: does not handle if there are two matches made at the same exact time (very low chance))
+        
+        wall = left_wall()
+        for starting_y in wall:
+            visited = bfs(starting_y)
+            if visited:
+                print('match made')
+                # delete the pixels
+                for x, y in visited:
+                    self.sand[y][x] = None
+                self.score += 100 # add on score value of making a match
+                return visited
+            
+        return None
